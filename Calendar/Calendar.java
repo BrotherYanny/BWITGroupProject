7cc3d8a51bf6905a0d99403f64554ace8c10580c
BrotherYanny <rachel.yan@warwick.ac.uk>
2020-11-01 14:10:18 +0000

    shuffled and added some comments

--- Calendar/Calendar.java
+++ Calendar/Calendar.java
@@ -30,6 +30,9 @@ public class Calendar {
               || (startTime.before(timePair.getKey()) && timePair.getValue().before(endTime))) { //time2 occurs during time1
                 System.out.println("User " + user + " has a meeting scheduled at this time. " + topic + " could not be scheduled.");
                 return;
+                // right now this interrupts the function, so if previous people in the list had the meeting successfully added,
+                // those meetings would not be cancelled -do we want this?
+                // maybe implement it so that all meetings are added at the end after everyone's schedules are checked?
               }
         }
         allCalendars.get(user).get(calendarDay).put(new Pair(startTime, endTime), topic);
@@ -41,7 +44,8 @@ public class Calendar {
   void displayUsersDay(int userID) {
     if (!allCalendars.containsKey(userID))
       return;
-    LinkedList<Pair<Pair<BTime,BTime>, String>> orderedDayCalendar = new LinkedList<>();
+    System.out.println("User " + userID + "'s calendar for today is as follows: ");
+    // LinkedList<Pair<Pair<BTime,BTime>, String>> orderedDayCalendar = new LinkedList<>();
     LocalDate date = LocalDate.now();
     //how to sort by time?
     if (allCalendars.containsKey(userID)) {
@@ -52,9 +56,10 @@ public class Calendar {
   }
 
   void displayUsersCalendarForGivenDay(int userID, LocalDate calendarDay) {
-    LinkedList<Pair<Pair<BTime,BTime>, String>> orderedDayCalendar = new LinkedList<>();
     if (!allCalendars.containsKey(userID))
       return;
+    System.out.println("User " + userID + "'s calendar for " + calendarDay + " is as follows: ");
+    // LinkedList<Pair<Pair<BTime,BTime>, String>> orderedDayCalendar = new LinkedList<>();
     for (Map.Entry<Pair<BTime, BTime>, String> timePair : allCalendars.get(userID).get(calendarDay).entrySet()) {
       System.out.println(timePair.getKey().getKey() + "-" + timePair.getKey().getValue() + ": " + timePair.getValue());
     }
@@ -62,28 +67,31 @@ public class Calendar {
 
   void meetingTimeSuggestion(int organisingUser, LocalDate calendarDay, BTime earliestTime, BTime latestTime,
   int timeInterval) { //assume timeInterval in minutes
-    if (!allCalendars.containsKey(organisingUser))
+    if (!allCalendars.containsKey(organisingUser)) {
+      System.out.println("Anytime between " + earliestTime.toString() + " and " + latestTime.toString() + " are ok.");
       return;
-        for (int i=0; i<=(earliestTime.difference(latestTime)-timeInterval)/30; i++) {
-        Pair<BTime, BTime> checkTime = new Pair(earliestTime.addHalfHour(i), earliestTime.addHalfHour(i+(timeInterval/30)));
-        Boolean timeOkay = true;
-        if (allCalendars.get(organisingUser).containsKey(calendarDay)) {
-          for (Pair<BTime, BTime> timePair : allCalendars.get(organisingUser).get(calendarDay).keySet()) {
-            if ((earliestTime.addHalfHour(i).getHour() == timePair.getKey().getHour() && earliestTime.addHalfHour(i).getMinute() == timePair.getKey().getMinute()) //start times are the same
-                || (earliestTime.addHalfHour(i+(timeInterval/30)).getHour() == timePair.getValue().getHour() && earliestTime.addHalfHour(i+(timeInterval/30)).getMinute() == timePair.getValue().getMinute()) //end times are the same
-                || (timePair.getKey().before(earliestTime.addHalfHour(i)) && earliestTime.addHalfHour(i).before(timePair.getValue())) //time1 starts during time2
-                || (timePair.getKey().before(earliestTime.addHalfHour(i+(timeInterval/30))) && earliestTime.addHalfHour(i+(timeInterval/30)).before(timePair.getValue())) //time1 ends during time2
-                || (earliestTime.addHalfHour(i).before(timePair.getKey()) && timePair.getValue().before(earliestTime.addHalfHour(i))) //time2 starts during time1
-                || (earliestTime.addHalfHour(i+(timeInterval/30)).before(timePair.getKey()) && timePair.getValue().before(earliestTime.addHalfHour(i+(timeInterval/30)))) //time2 ends during time1
-                || (timePair.getKey().before(earliestTime.addHalfHour(i)) && earliestTime.addHalfHour(i+(timeInterval/30)).before(timePair.getValue())) //time1 occurs during time2
-                || (earliestTime.addHalfHour(i).before(timePair.getKey()) && timePair.getValue().before(earliestTime.addHalfHour(i+(timeInterval/30))))) { //time2 occurs during time1
-                  timeOkay = false;
-                  break;
-                }
-          }
-        }
-        if (timeOkay)
-          System.out.println(earliestTime.addHalfHour(i) + "-" + earliestTime.addHalfHour(i+(timeInterval/30)));
+    }
+    System.out.println("The recommended meeting times are: ");
+    for (int i=0; i<=(earliestTime.difference(latestTime)-timeInterval)/30; i++) {
+    Pair<BTime, BTime> checkTime = new Pair(earliestTime.addHalfHour(i), earliestTime.addHalfHour(i+(timeInterval/30)));
+    Boolean timeOkay = true;
+    if (allCalendars.get(organisingUser).containsKey(calendarDay)) {
+      for (Pair<BTime, BTime> timePair : allCalendars.get(organisingUser).get(calendarDay).keySet()) {
+        if ((earliestTime.addHalfHour(i).getHour() == timePair.getKey().getHour() && earliestTime.addHalfHour(i).getMinute() == timePair.getKey().getMinute()) //start times are the same
+            || (earliestTime.addHalfHour(i+(timeInterval/30)).getHour() == timePair.getValue().getHour() && earliestTime.addHalfHour(i+(timeInterval/30)).getMinute() == timePair.getValue().getMinute()) //end times are the same
+            || (timePair.getKey().before(earliestTime.addHalfHour(i)) && earliestTime.addHalfHour(i).before(timePair.getValue())) //time1 starts during time2
+            || (timePair.getKey().before(earliestTime.addHalfHour(i+(timeInterval/30))) && earliestTime.addHalfHour(i+(timeInterval/30)).before(timePair.getValue())) //time1 ends during time2
+            || (earliestTime.addHalfHour(i).before(timePair.getKey()) && timePair.getValue().before(earliestTime.addHalfHour(i))) //time2 starts during time1
+            || (earliestTime.addHalfHour(i+(timeInterval/30)).before(timePair.getKey()) && timePair.getValue().before(earliestTime.addHalfHour(i+(timeInterval/30)))) //time2 ends during time1
+            || (timePair.getKey().before(earliestTime.addHalfHour(i)) && earliestTime.addHalfHour(i+(timeInterval/30)).before(timePair.getValue())) //time1 occurs during time2
+            || (earliestTime.addHalfHour(i).before(timePair.getKey()) && timePair.getValue().before(earliestTime.addHalfHour(i+(timeInterval/30))))) { //time2 occurs during time1
+              timeOkay = false;
+              break;
+            }
+      }
+    }
+    if (timeOkay)
+      System.out.println(earliestTime.addHalfHour(i) + "-" + earliestTime.addHalfHour(i+(timeInterval/30)));
     }
   }
 }
--- Calendar/Test.java
+++ Calendar/Test.java
@@ -4,25 +4,13 @@ import java.time.*;
 
 public class Test {
 
-  // public static Map<Integer, User> userList = new HashMap<>();
-
   public static void main(String[] args) {
 
-
-    User rachel = new User("Rachel Yan", "yanrachel.nor@gmail.com", "07776825032");
-
     Calendar cal = new Calendar();
-    // userList.put(rachel.getID(), rachel);
-
-    System.out.println(rachel.getName());
-    // System.out.println(rachel.getEmail());
-    // System.out.println(rachel.getPhoneNo());
-    System.out.println(rachel.getJoiningDay());
-
-    System.out.println("-----------------------------------------------------------------------");
 
     List<Integer> meetingParticipants = new ArrayList<>();
     meetingParticipants.add(1);
+    meetingParticipants.add(2);
 
     cal.addMeeting(meetingParticipants, LocalDate.now(), new BTime(14, 00), new BTime(14, 30), "vibe");
     cal.addMeeting(meetingParticipants, LocalDate.now(), new BTime(14, 00), new BTime(14, 30), "vibe2");
